c76b9ac1841d0f791ddd17f85b83f278c8d85b91
Hass <hassmroueh@gmail.com>
2024-04-01 03:12:34 -0400

    mini project finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,36 @@
+
+
 import React from "react";
+import Header from "./Header"
+import ArticleList from "./ArticleList";
+import About from "./About";
+import Article from "./Article";
 import blogData from "../data/blog";
 
-console.log(blogData);
 
+
+
+console.log(blogData);
+const blogName = "Underreacted"
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+       <Header name={blogName} />
+       <About
+        image="https://via.placeholder.com/215"
+        about="About this Blog."
+      />
+     <ArticleList posts={blogData.posts} />
+     <Article
+  title={"Components 101"}
+  date={"December 15, 2020"}
+  preview={"Setting up the building blocks of your site"}
+/>
+
+
     </div>
   );
 }
+
 
 export default App;
